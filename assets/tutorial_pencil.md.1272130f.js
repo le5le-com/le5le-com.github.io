4edec4ddaee0a2bf49f193ce16c1a1fdc451fc9a
SignDawn <48878568+SignDawn@users.noreply.github.com>
2022-03-30 03:36:34 +0000

    Deploying to gh-pages from  @ 5302e5ce80955f22fc7f0c5d49d24a781a9a29c7 🚀

--- assets/tutorial_pencil.md.1272130f.js
+++
@@ -1,8 +0,0 @@
-import{_ as n,c as a,o as t,a as s}from"./app.d493b7ab.js";var e="/topology-documents/img/pencil.png";const h='{"title":"\u94C5\u7B14","description":"","frontmatter":{},"relativePath":"tutorial/pencil.md","lastUpdated":1648606104014}',o={},p=s('<h1 id="\u94C5\u7B14" tabindex="-1">\u94C5\u7B14 <a class="header-anchor" href="#\u94C5\u7B14" aria-hidden="true">#</a></h1><p>\u94C5\u7B14\u662F\u53EF\u4EE5\u4EFB\u610F\u6D82\u9E26\u7684\u7ED8\u56FE\u5C0F\u5DE5\u5177</p><p><img src="'+e+`" alt="\u4E50\u543E\u4E50topology\u94C5\u7B14\u5DE5\u5177"></p><p>\u5728\u7EBF\u4F53\u9A8C\uFF1A <a href="http://t.le5le.com/" target="_blank" rel="noopener noreferrer">http://t.le5le.com/</a></p><p>\u793A\u4F8B\uFF1A</p><div class="language-js"><pre><code><span class="token comment">// \u5F00\u59CB\u94C5\u7B14\u7ED8\u753B</span>
-topology<span class="token punctuation">.</span><span class="token function">drawingPencil</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
-
-<span class="token comment">// \u9F20\u6807\u62AC\u8D77\u7ED3\u675F</span>
-
-<span class="token comment">// \u505C\u6B62\u94C5\u7B14\u7ED8\u753B\uFF08\u5173\u95ED\u94C5\u7B14\u7ED8\u753B\u72B6\u6001\uFF09</span>
-topology<span class="token punctuation">.</span><span class="token function">stopPencil</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
-</code></pre></div>`,6),c=[p];function l(i,r,_,u,d,m){return t(),a("div",null,c)}var f=n(o,[["render",l]]);export{h as __pageData,f as default};
